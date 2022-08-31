bc5fdc9c874e38a3fc30649c5cd9c19dce4f3090
isabellepimentel <isabellechavespimentel@gmail.com>
2022-08-31 15:51:37 -0300

    Exercicio Strings

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+let nome = prompt("Qual seu nome?")
+let comida1 = prompt("Qual sua comida favorita?")
+let comida2 = prompt("Qual sua segunda comida favorita?")
+let comida3 = prompt("Qual sua terceira comida favorita?")
+
+console.log(`
+Estas são as comidas favoritas de ${nome}
+- ${comida1}
+- ${comida2}
+- ${comida3}
+`)
